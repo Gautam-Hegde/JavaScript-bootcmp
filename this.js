a0e5c27b13f9ff0039d37c87ccdbe98ae1345c76
Gautam-Hegde <gautamatjob@protonmail.com>
2021-09-26 20:50:24 +0530

    initial JS

---
+++ this.js
@@ -0,0 +1,8 @@
+// //some console commands.
+// console.time('random label');
+// // console.log('hello world');
+
+// // console.warn('this is a warning');
+// // console.error('this is an error');
+// // console.clear();
+// console.timeEnd('random label');
